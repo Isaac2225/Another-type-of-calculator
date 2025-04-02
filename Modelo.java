d96a71992068139a1f49c77eddd2320e15a49d39
Isaac22 <isaacgomezvallejos@gmail.com>
2025-04-02 09:43:14 +0200

    Create Modelo.java

---
+++ Modelo.java
@@ -0,0 +1,17 @@
+public class Modelo {
+    public int suma(int n, int n2){
+        return n+n2;
+    }
+
+    public int resta(int n, int n2){
+        return n-n2;
+    }
+
+    public int multiplicacion(int n, int n2){
+        return n*n2;
+    }
+
+    public double division(int n, int n2){
+        return n/n2;
+    }
+}
